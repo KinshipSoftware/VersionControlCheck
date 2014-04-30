6b76d1f9d76636096a2cfca137f38823a401e84d
Peter Withers <Peter.Withers@mpi.nl>
2014-04-30 16:33:29 +0200

    Added the version number checks and verbose output. Added various parameters such as build type and modules with short ids.

--- src/main/java/nl/mpi/tla/version/controller/VersionControlCheck.java
+++ src/main/java/nl/mpi/tla/version/controller/VersionControlCheck.java
@@ -1,19 +1,20 @@
 /**
- * Copyright (C) 2014 The Language Archive, Max Planck Institute for Psycholinguistics
+ * Copyright (C) 2014 The Language Archive, Max Planck Institute for
+ * Psycholinguistics
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.tla.version.controller;
 
@@ -42,6 +43,10 @@ import org.slf4j.LoggerFactory;
  */
 public class VersionControlCheck extends AbstractMojo {
 
+    enum BuildType {
+
+        pretesting, testing, stable, SNAPSHOT
+    }
     final private Logger logger = LoggerFactory.getLogger(getClass());
     /**
      * @parameter expression="${project}"
@@ -68,18 +73,67 @@ public class VersionControlCheck extends AbstractMojo {
      * @readonly
      */
     private List<MavenProject> mavenProjects;
+    /**
+     * @parameter expression="${majorVersion}"
+     * @required
+     * @readonly
+     */
+    private String majorVersion;
+    /**
+     * @parameter expression="${minorVersion}"
+     * @required
+     * @readonly
+     */
+    private String minorVersion;
+    /**
+     * @parameter expression="${buildType}"
+     * @required
+     * @readonly
+     */
+    private BuildType buildType;
+    /**
+     * Modules which are allowed short module versions: eg 1.0 instead of
+     * 1.0.0-testing.
+     *
+     * @parameter alias="shortVersionModules"
+     * @readonly
+     */
+    private List<String> modulesWithShortVersion;
+
+    /**
+     * @parameter expression="${verbose}"
+     * @required
+     * @readonly
+     */
+    private boolean verbose;
+    /**
+     * @parameter expression="${allowSnapshots}"
+     * @required
+     * @readonly
+     */
+    private boolean allowSnapshots;
 
     @Override
     public void execute() throws MojoExecutionException {
-        logger.info("VersionControlCheck");
-        logger.info("project: " + project);
-        logger.info("outputDirectory: " + outputDirectory);
-        logger.info("projectDirectory :" + projectDirectory);
+        if (verbose) {
+            logger.info("VersionControlCheck");
+            logger.info("project: " + project);
+            logger.info("majorVersion: " + majorVersion);
+            logger.info("minorVersion: " + minorVersion);
+            logger.info("buildType: " + buildType);
+            logger.info("outputDirectory: " + outputDirectory);
+            logger.info("projectDirectory :" + projectDirectory);
+        }
         try {
-//        for (String moduleName : new String[]{"rabbit"}) {
             for (MavenProject reactorProject : mavenProjects) {
-                logger.info("moduleId: " + reactorProject.getArtifactId());
-                logger.info("moduleDir :" + reactorProject.getBasedir());
+                final String artifactId = reactorProject.getArtifactId();
+                final String moduleVersion = reactorProject.getVersion();
+                logger.info("Checking version numbers for " + artifactId);
+                if (verbose) {
+                    logger.info("artifactId: " + artifactId);
+                    logger.info("moduleVersion: " + moduleVersion);
+                    logger.info("moduleDir :" + reactorProject.getBasedir());
+                }
 //                for (MavenProject dependencyProject : reactorProject.getDependencies()) {
 //                    logger.info("dependencyProject:" + dependencyProject.getArtifactId());
 //                }
@@ -89,14 +143,28 @@ public class VersionControlCheck extends AbstractMojo {
                 while (scanner.hasNext()) {
                     final String nextLine = scanner.nextLine();
                     if (nextLine.indexOf(" ") != 40) {
-                        logger.info(nextLine);
+                        if (verbose) {
+                            logger.info(nextLine);
+                        }
 //                        logger.info(nextLine.indexOf(" "));
                         throw new MojoExecutionException("Invalid git log found. Please check the project directory specified is a git repository.");
                     }
                     lineCount++;
                 }
-                logger.info(reactorProject.getArtifactId() + ".buildVersion: " + Integer.toString(lineCount));
-                reactorProject.getProperties().setProperty(reactorProject.getArtifactId() + ".buildVersion", Integer.toString(lineCount));
+                logger.info(artifactId + ".buildVersion: " + Integer.toString(lineCount));
+                final String expectedVersion;
+                if (modulesWithShortVersion != null && modulesWithShortVersion.contains(artifactId)) {
+                    expectedVersion = majorVersion + "." + minorVersion;
+                } else {
+                    expectedVersion = majorVersion + "." + minorVersion + "." + lineCount + "-" + buildType;
+                }
+                if (!expectedVersion.equals(moduleVersion)) {
+                    logger.error("Expecting version number: " + expectedVersion);
+                    logger.error("But found: " + moduleVersion);
+                    logger.error("Artifact: " + artifactId);
+                    throw new MojoExecutionException("The build numbers to not match for '" + artifactId + "': '" + expectedVersion + "' vs '" + moduleVersion + "'");
+                }
+                reactorProject.getProperties().setProperty(artifactId + ".buildVersion", Integer.toString(lineCount));
             }
         } catch (IOException exception) {
             throw new MojoExecutionException("Failed to get the git log.", exception);
